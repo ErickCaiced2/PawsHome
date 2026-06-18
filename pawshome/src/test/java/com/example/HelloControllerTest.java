ebc70265adda5d0104fffe1ed2079e58f4e10dbc
ErickCaiced2 <erick.caicedo@epn.edu.ec>
2026-06-18 15:45:30 -0500

    Inicio Springboot

---
+++ pawshome/src/test/java/com/example/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHello() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
+    }
+
+    @Test
+    void testCalc() throws Exception {
+        mockMvc.perform(get("/calc")
+                        .param("left", "100")
+                        .param("right", "200"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.left").value(100))
+                .andExpect(jsonPath("$.right").value(200))
+                .andExpect(jsonPath("$.answer").value(300));
+    }
+}
